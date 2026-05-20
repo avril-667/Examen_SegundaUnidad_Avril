6d8ca606373b85f97448b528fa6b3ad6a670a167
avril palencia <avrilpalencia2011@gmail.com>
2026-05-20 11:03:51 -0600

    first commit

---
+++ src/SistemaDeGestion.java
@@ -0,0 +1,12 @@
+public class SistemaDeGestion {
+     java.util.Scanner;
+    public static void main(String[] args){
+         //carnet y grado :)
+        class Estudiante extends {
+            private int carnet;
+            public Estudiante(String grado, int carnet){
+
+            }
+        }
+    }
+}
